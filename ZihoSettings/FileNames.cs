8b6361c75d3c746e7fa0f3b1c586b15ea39077ef
DESKTOP2022\h195m <h195mic@gmail.com>
2023-02-14 11:39:56 +0900

    setting.jsonを参照するように変更

--- ZihoSettings/FileNames.cs
+++ ZihoSettings/FileNames.cs
@@ -5,107 +5,121 @@ using System.Text.RegularExpressions;
 using System.Drawing.Text;
 using System.DirectoryServices.ActiveDirectory;
 using System.Diagnostics;
+using System.Text.Json;
 
-//App.configを取得・編集するクラス
+//setting.json
+public class VoiceSettings
+{
+    public string? TaskName { get; set; }
+    public int Volume { get; set; }
+    public string? FolderPath { get; set; }
+    public string[] VoicePath { get; set; }
+    public bool[]? Sleep { get; set; }
+
+    public VoiceSettings()
+    {
+        this.VoicePath = new string[24];
+    }
+}
 public class FileNames
 {
-	private string[] _fileList;
-	Configuration config;
+    string _settingFile = "setting.json";
+    string[] _fileList = new string[24];
+    VoiceSettings _voiceSettings;
 
     public FileNames()
-	{
-        this.config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-        this._fileList = new string[24];
-        for (int i = 0; i < 24; i++)
+    {
+        string jsonstr = File.ReadAllText(this._settingFile);
+        this._voiceSettings = JsonSerializer.Deserialize<VoiceSettings>(jsonstr)!;
+        if (this._voiceSettings.VoicePath is not null)
         {
-            string key = "key" + i.ToString();
-            string? file = ConfigurationManager.AppSettings[key];
-            if (file is not null) _fileList[i] = file;
-            else _fileList[i] = "noFile";
+            for (int i = 0; i < 24; i++)
+            {
+                if (this._voiceSettings.VoicePath[i] is not null) _fileList[i] = this._voiceSettings.VoicePath[i];
+                else _fileList[i] = "noFile";
+            }
         }
     }
 
-    public string[]? FileList { get; }
+    internal string GetFileList(int hour)
+    {
+        return _fileList[hour];
+    }
 
-	internal string GetFileList(int hour)
-	{
-		return this._fileList[hour];
-	}
+    internal void SetFileList(int hour, string filePath)
+    {
+        _fileList[hour] = filePath;
+    }
 
-	internal void SetFileList(int hour, string filePath)
-	{
-		_fileList[hour] = filePath;
-	}
+    //フォルダを参照して_fileListを作成する関数
+    internal void BrowseFileList(string path)
+    {
+        string[] fileList = new string[25];
+        var filesMp3 = Directory.GetFiles(path, "*.mp3", SearchOption.AllDirectories).ToList();
+        var filesWav = Directory.GetFiles(path, "*.wav", SearchOption.AllDirectories).ToList();
+        filesMp3.AddRange(filesWav);
 
-	//フォルダを参照して_fileListを作成する関数
-	internal void BrowseFileList(string path)
-	{
-		string[] fileList = new string[25];
-		string[] files = Directory.GetFiles(path, "*.mp3", SearchOption.AllDirectories);
-		if (files.Length == 0) return;
+        string[] files = filesMp3.ToArray();
+
+        if (files.Length == 0) return;
 
         //各ファイルの数字の部分を抽出
         foreach (string file in files)
-		{
-			string fileName = Path.GetFileName(file);
-			Match match = Regex.Match(fileName, "[0-2][0-9]|[0-9]"); //先に二桁の判定が行われる(検証済み)
-			int h = int.Parse(match.Value);
-			fileList[h] = file;
-		}
+        {
+            string fileName = Path.GetFileName(file);
+            Match match = Regex.Match(fileName, "[0-2][0-9]|[0-9]"); //先に二桁の判定が行われる(検証済み)
+            int h = int.Parse(match.Value);
+            fileList[h] = file;
+        }
 
-		//午後(12～24時)がない場合
-		for (int i=12; i<=24; i++)
-		{
-			if (fileList[i - 12] is not null & fileList[i] is null) fileList[i] = fileList[i - 12];
-		}
+        //午後(12～24時)がない場合
+        for (int i = 12; i <= 24; i++)
+        {
+            if (fileList[i - 12] is not null & fileList[i] is null) fileList[i] = fileList[i - 12];
+        }
 
-		//0時のファイルが存在せず24時のファイルが存在する場合
-		if (fileList[0] is null && fileList[24] is not null) fileList[0] = fileList[24];
+        //0時のファイルが存在せず24時のファイルが存在する場合
+        if (fileList[0] is null && fileList[24] is not null) fileList[0] = fileList[24];
 
-		for (int i=0; i<24; i++)
-		{
-			if (fileList[i] is not null)
-			{
-				_fileList[i] = fileList[i];
-			}
-			else
-			{
-				_fileList[i] = "noFile(ブラウズファイルリスト)";
-			}
-		}
-	}
-
-	//AppConfigに音声設定を保存する
-	internal void SaveConfigFileList()
-	{
-		for (int i=0; i<24; i++)
-		{
-			if (_fileList[i] == "noFile" || _fileList[i] is null)
-			{
-				SetConfigFileList(i, "noFile");
-			}
-			else
-			{
-                SetConfigFileList(i, _fileList[i]);
+        for (int i = 0; i < 24; i++)
+        {
+            if (fileList[i] is not null)
+            {
+                _fileList[i] = fileList[i];
             }
-		}
-        config.Save();
+            else
+            {
+                _fileList[i] = "noFile(ブラウズファイルリスト)";
+            }
+        }
     }
 
-    private void SetConfigFileList(int i, string filePath)
-	{
-		string key = "key" + i.ToString();
-		config.AppSettings.Settings[key].Value = filePath;
-	}
-
-	public void DeleteConfigFileList()
-	{
-		for (int i=0; i<24; i++)
-		{
-            string key = "key" + i.ToString();
-            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            config.AppSettings.Settings[key].Value = "noFile";
-            _fileList[i] = "noFile";
+    //setting.jsonに音声設定を保存する
+    internal void SaveConfigFileList()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            if (_fileList[i] == "noFile" || _fileList[i] is null)
+            {
+                _voiceSettings.VoicePath[i] = "noFile";
+            }
+            else
+            {
+                _voiceSettings.VoicePath[i] = _fileList[i];
+            }
         }
-	}
+        string jsonStr = JsonSerializer.Serialize(_voiceSettings);
+        File.WriteAllText(_settingFile, jsonStr);
+    }
+
+    public void DeleteConfigFileList()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            _fileList[i] = "noFIle";
+            _voiceSettings.VoicePath[i] = "noFile";
+        }
+        string jsonStr = JsonSerializer.Serialize(_voiceSettings);
+        File.WriteAllText(_settingFile, jsonStr);
+    }
 }
